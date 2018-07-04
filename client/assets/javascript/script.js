77778d615617f3ddae19d2c5f2a0d7ce4c1e6dd4
Preston Powell <prestonpowell@Prestons-MacBook-Pro.local>
2018-07-04 00:39:49 -0700

    got post method working and data is coming to the backend

--- client/assets/javascript/script.js
+++ client/assets/javascript/script.js
@@ -1,18 +1,13 @@
 
 const $ = window.$
-const alert = window.alert
-let files = window.files
+// const alert = window.alert
+// let files = window.files
+
 $(document).ready(() => {
-  $('#txtFileUpload').on('click', () => {
-    // event.preventDefault()
-    alert('you clicked me you piece of shit')
+  $('.fileSubmit').on('click', () => {
+      $.ajax({
+          url: '/upload',
+          method: 'post'
+      })
   })
-
-  $('input[type=file]').on('change', prepareUpload)
-
-  // Grab the files and set them to our variable
-  function prepareUpload (event) {
-    files = event.target.files
-    console.log(files)
-  }
 })
--- server.js
+++ server.js
@@ -12,17 +12,12 @@ app.get('/', (req, res) => {
 })
 
 app.post('/upload', (req, res) => {
-  if (!req.files) { return res.status(400).send('No files were uploaded.')}
-
+  if (!req.files) { return res.status(400).send('No files were uploaded.') }
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let sampleFile = req.files.sampleFile
+  console.log(sampleFile)
 
-  // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv('/somewhere/on/your/server/filename.jpg', function (err) {
-    if (err) { return res.status(500).send(err)}
-
-    res.send('File uploaded!')
-  })
+  res.send('File uploaded!')
 })
 
 app.listen(PORT, () => {
